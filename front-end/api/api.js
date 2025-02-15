61ce6a9a70487de8b97d2e7d0fed1b8ddcff8d87
JessicaLima521 <jessicaambroziodelima@gmail.com>
2025-02-15 13:17:04 -0300

    novos ajuste plays

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 //const {NODE_ENV}=process.env;
 //const URL= NODE_ENV === "http://localhost:3001/api";
-const URL= NODE_ENV === "https://deploy-full-stack-jksg.onrender.com/api";
+const URL= "https://deploy-full-stack-jksg.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
