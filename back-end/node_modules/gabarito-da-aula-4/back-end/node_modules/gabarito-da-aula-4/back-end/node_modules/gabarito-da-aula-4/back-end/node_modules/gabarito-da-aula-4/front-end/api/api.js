4f782de978962146ef7447aa492158506dd14157
JessicaLima521 <jessicaambroziodelima@gmail.com>
2025-02-15 13:00:06 -0300

    novos ajustes

--- back-end/node_modules/gabarito-da-aula-4/back-end/node_modules/gabarito-da-aula-4/back-end/node_modules/gabarito-da-aula-4/back-end/node_modules/gabarito-da-aula-4/front-end/api/api.js
+++ back-end/node_modules/gabarito-da-aula-4/back-end/node_modules/gabarito-da-aula-4/back-end/node_modules/gabarito-da-aula-4/back-end/node_modules/gabarito-da-aula-4/front-end/api/api.js
@@ -2,8 +2,9 @@
 import 'dotenv/config'
 import axios from "axios";
 
-const {NODE_ENV}=process.env;
-const URL= NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+//const {NODE_ENV}=process.env;
+//const URL= NODE_ENV === "http://localhost:3001/api";
+const URL= NODE_ENV === "https://deploy-full-stack-jksg.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
